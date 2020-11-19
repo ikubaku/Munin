abf19297b44e4bd0ceeb5c9594ace84b2c20ad59
ikubaku <hide4d51@gmail.com>
2020-11-20 01:54:44 +0900

    Show the failure rate

--- analyzer.py
+++ analyzer.py
@@ -23,14 +23,19 @@ class Analyzer:
         lib_info_list = self.database.get_library_info_list()
         n_libs = len(lib_info_list)
         print('Looking for library headers from {} libraries...'.format(n_libs))
+        n_failure = 0
         bar = Bar('PROGRESS', max=n_libs)
         for lib_info in self.database.get_library_info_list():
             headers = self.get_headers_for_library(lib_info.path)
             if headers is None:
                 logging.warning('Analysis failed with the library: {}-{}'.format(lib_info.name, lib_info.version))
+                n_failure += 1
             else:
                 self.database.add_header_dictionary_entry(lib_info, headers)
             bar.next()
+        if n_failure > 0:
+            print()
+            print('{} out of {} analyses failed. Consult the log file for more information.'.format(n_failure, n_libs))
 
     def get_headers_for_library(self, library_path):
         logging.info('Analyzing the library in path: {}...'.format(library_path))
@@ -79,7 +84,7 @@ class Analyzer:
             headers = []
             # Try the source directory
             for f in filenames:
-                if re.fullmatch(r'^[^/]+/src/[^/]+.h$', f):
+                if re.fullmatch(r'^[^/]+/src/[^/]+.(h|hpp)$', f):
                     headers.append(str(Path(f).name))
             if len(headers) > 0:
                 return headers
@@ -87,7 +92,7 @@ class Analyzer:
             logging.info('No headers in the src directory.')
             logging.info('Trying the packages root directory...')
             for f in filenames:
-                if re.fullmatch(r'^[^/]+/[^/]+.h$', f):
+                if re.fullmatch(r'^[^/]+/[^/]+.(h|hpp)$', f):
                     headers.append(str(Path(f).name))
             if len(headers) == 0:
                 logging.warning('No header file found for the library with path: {}'.format(library_path))
