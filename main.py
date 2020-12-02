74c7e7b6d63a82b26f0757e560e1501044eaa628
ikubaku <hide4d51@gmail.com>
2020-12-03 04:38:13 +0900

    Remove unnecessary comments

--- main.py
+++ main.py
@@ -111,5 +111,3 @@ def main():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     sys.exit(main())
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
